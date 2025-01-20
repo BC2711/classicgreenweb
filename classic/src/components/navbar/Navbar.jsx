2a388dfaaec042198400b9fdfbb81cb2d1e75dc3
Naushad145 <41935795+Naushad145@users.noreply.github.com>
2025-01-20 08:34:34 +0200

    updates

--- classic/src/components/navbar/Navbar.jsx
+++ classic/src/components/navbar/Navbar.jsx
@@ -1,35 +1,115 @@
-import './Navbar.css'
-import logo from '../../assets/logoclassic.jpg'
-import { useEffect, useState } from 'react'
+import './Navbar.css';
+import logo from '../../assets/logoclassic.jpg';
+import { useEffect, useState } from 'react';
 import { Link, animateScroll as scroll, scrollSpy } from 'react-scroll';
 import menu from '../../assets/menu.png';
+
 const Navbar = () => {
     const [sticky, setSticky] = useState(false);
-    useEffect(() => {
-        window.addEventListener('scroll', () => {
-            window.scrollY > 50 ? setSticky(true) : setSticky(false);
-        });
-    }, [])
-
     const [mobile, setMobile] = useState(false);
-    const hideMenu = () => {
-        mobile ? setMobile(false) : setMobile(true);
-    }
+
+    // Add event listener for sticky navbar
+    useEffect(() => {
+        const handleScroll = () => setSticky(window.scrollY > 50);
+        window.addEventListener('scroll', handleScroll);
+
+        // Initialize scrollSpy to monitor scrolling
+        scrollSpy.update();
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const toggleMenu = () => setMobile(!mobile);
+
     return (
         <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
             <img src={logo} alt="classicgreen" className='logo' />
             <ul className={`${mobile ? '' : 'hide-menu'}`}>
-                <li> <Link to='hero' smooth={true} offset={0} duration={500}>Home</Link> </li>
-                <li><Link to='features' smooth={true} offset={-70} duration={500}>Features</Link></li>
-                <li><Link to='portfolio' smooth={true} offset={-80} duration={500}>Portfolio</Link></li>
-                <li><Link to='faq' smooth={true} offset={0} duration={500}>FAQ</Link></li>
-                <li><Link to='about' smooth={true} offset={0} duration={500}>About</Link></li>
-                <li><Link to='contact' smooth={true} offset={-250} duration={500}>Contact</Link></li>
-                <li><button className='btn'>Signup</button></li>
+                <li>
+                    <Link 
+                        to='hero' 
+                        smooth={true} 
+                        offset={0} 
+                        duration={500} 
+                        activeClass="active"
+                        spy={true}
+                    >
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link 
+                        to='features' 
+                        smooth={true} 
+                        offset={-70} 
+                        duration={500} 
+                        activeClass="active"
+                        spy={true}
+                    >
+                        Features
+                    </Link>
+                </li>
+                <li>
+                    <Link 
+                        to='portfolio' 
+                        smooth={true} 
+                        offset={-80} 
+                        duration={500} 
+                        activeClass="active"
+                        spy={true}
+                    >
+                        Portfolio
+                    </Link>
+                </li>
+                <li>
+                    <Link 
+                        to='faq' 
+                        smooth={true} 
+                        offset={0} 
+                        duration={500} 
+                        activeClass="active"
+                        spy={true}
+                    >
+                        FAQ
+                    </Link>
+                </li>
+                <li>
+                    <Link 
+                        to='about' 
+                        smooth={true} 
+                        offset={0} 
+                        duration={500} 
+                        activeClass="active"
+                        spy={true}
+                    >
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link 
+                        to='contact' 
+                        smooth={true} 
+                        offset={-250} 
+                        duration={500} 
+                        activeClass="active"
+                        spy={true}
+                    >
+                        Contact
+                    </Link>
+                </li>
+                <li>
+                    <button className='btn'>Signup</button>
+                </li>
             </ul>
-            <img src={menu} alt="menu" className='menu' onClick={hideMenu} />
+            <img
+                src={menu}
+                alt="menu"
+                className='menu'
+                onClick={toggleMenu}
+                aria-label="Toggle menu"
+            />
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
